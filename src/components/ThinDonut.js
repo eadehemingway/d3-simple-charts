844c621409a3666b856ea9044c1c52d2bf5148ae
eadehemingway <eade.hemingway@gmail.com>
2019-08-19 13:00:19 +0300

    tidy thin donut

--- src/components/ThinDonut.js
+++ src/components/ThinDonut.js
@@ -3,13 +3,12 @@ import * as d3 from 'd3';
 
 export class ThinDonut extends React.Component {
   state = {
-    data: [1, 2]
+    data: []
   };
 
-
   componentDidMount() {
-    const { data } = this.state
-    this.interval = setInterval(() => this.setState({ data: this.makeData(2) }), 2000);
+    this.setState({ data: this.makeData(20) })
+    this.interval = setInterval(() => this.setState({ data: this.makeData(20) }), 1500);
     const width = 960
     const height = 500;
 
@@ -18,76 +17,57 @@ export class ThinDonut extends React.Component {
       .attr("width", width)
       .attr("height", height);
 
-    const donutContainer = svg
+    svg
       .append('g')
       .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
       .attr('class', 'donutContainer')
-    const oRadius = 200;
-    const iRadius = 180;
-    const color = ['lightslategrey', 'linen']
-    const arc = d3.arc()
-      .outerRadius(oRadius)
-      .innerRadius(iRadius);
-    const pie = d3.pie().value(function (d) { return d; }).sort(null);
 
-    donutContainer
-      .selectAll("path")
-      .data(pie(data))
-      .enter()
-      .append("path")
-      .attr("class", "piechart")
-      .attr("fill", (d, i) => color[i])
-      .transition()
-      .duration(1500)
-      .attrTween("d", function (d) {
-        const i = d3.interpolate(0, d);
-        this._current = i(0);
-        return function (t) {
-          return arc(i(t));
-        };
-
-      })
-
+    this.drawDonut()
 
   }
 
   componentDidUpdate() {
     this.drawDonut()
   }
+
   drawDonut = () => {
     const { data } = this.state
 
-    const oRadius = 200;
-    const iRadius = 180;
-
     const arc = d3.arc()
-      .outerRadius(oRadius)
-      .innerRadius(iRadius);
-    const pie = d3.pie().value(function (d) { return d; }).sort(null);
+      .outerRadius(200)
+      .innerRadius(180);
+    const pie = d3.pie().value((d) => d).sort(null);
     const donutContainer = d3.select('.donutContainer')
-    // enter data and draw pie chart
 
     const arcSelection = donutContainer
       .selectAll("path")
       .data(pie(data))
 
-    arcSelection
+    const enterSelection = donutContainer
+      .selectAll("path")
+      .data(pie(data))
+      .enter()
+      .append("path")
+      .attr("fill", 'lightslategrey')
+      .attr('stroke', 'linen')
+      .attr('stroke-width', '3px')
+
+    const updateSelection = enterSelection.merge(arcSelection)
+
+    updateSelection
       .transition()
       .duration(1500)
       .attrTween("d", function (d) {
-        const i = d3.interpolate(this._current, d);
-        this._current = i(0);
-        return function (t) {
-          return arc(i(t));
-        };
+        const prevData = this._prevData || 0
+        const i = d3.interpolate(prevData, d);
+        this._prevData = d;
+        return (t) => arc(i(t))
 
       })
 
   }
   makeData = (size) => {
-    return d3.range(size).map(function (item) {
-      return Math.random() * 100;
-    });
+    return d3.range(size).map(() => Math.random() * 100);
   };
   componentWillUnmount() {
     clearInterval(this.interval);
