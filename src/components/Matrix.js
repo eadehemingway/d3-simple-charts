164e84360d25af0d06b5b214ad26ebe923aa6091
eadehemingway <eade.hemingway@gmail.com>
2019-09-12 15:39:26 +0300

    make different columns of dots work

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -15,11 +15,25 @@ export class Matrix extends React.Component {
       { id: 12, campus: 'gaza', gender: 'male', role: 'student' },
       { id: 14, campus: 'gaza', gender: 'female', role: 'mentor' },
       { id: 15, campus: 'gaza', gender: 'female', role: 'mentor' },
-      { id: 16, campus: 'gaza', gender: 'female', role: 'student' }
+      { id: 16, campus: 'gaza', gender: 'female', role: 'student' },
+      { id: 17, campus: 'gaza', gender: 'male', role: 'student' },
+      { id: 18, campus: 'gaza', gender: 'female', role: 'mentor' },
+      { id: 19, campus: 'gaza', gender: 'female', role: 'mentor' },
+      { id: 20, campus: 'gaza', gender: 'female', role: 'student' },
+      { id: 21, campus: 'gaza', gender: 'male', role: 'student' },
+      { id: 22, campus: 'gaza', gender: 'female', role: 'mentor' },
+      { id: 23, campus: 'gaza', gender: 'female', role: 'mentor' },
+      { id: 24, campus: 'gaza', gender: 'female', role: 'student' },
+      { id: 25, campus: 'london', gender: 'female', role: 'mentor' },
+      { id: 26, campus: 'london', gender: 'female', role: 'mentor' },
+      { id: 27, campus: 'london', gender: 'female', role: 'student' }
     ]
   }
+  // getX1Coordinate = (d) => {
 
-  getXCoordinate = (index, dotsPerRow, circleWidth) => {
+  // }
+
+  getX2Coordinate = (index, dotsPerRow, circleWidth) => {
     const placeInRow = index % dotsPerRow
     const circlePadding = 5
     const boxPadding = 50
@@ -31,20 +45,45 @@ export class Matrix extends React.Component {
     const boxPadding = 50
     return placeInCol * (circlePadding + circleWidth) + boxPadding
   }
+  campusXCoordinate = campus => {
+    switch (campus) {
+      case 'gaza':
+        return 0
+      case 'khaleel':
+        return 200
+      case 'london':
+        return 400
+    }
+  }
+
   componentDidMount() {
     const { data } = this.state
 
     d3.select('svg')
+      .attr('width', 700)
+      .attr('height', 500)
       .selectAll('circle')
       .data(data)
       .enter()
       .append('circle')
       .attr('r', 5)
-      .attr('cy', (d, i) => this.getYCoordinate(i, 4, 10))
-      .attr('cx', (d, i) => this.getXCoordinate(i, 4, 10))
+      .attr('cy', (d, i) => {
+        const index = this.findIndexOfDataInGroup(d)
+        return this.getYCoordinate(index, 4, 10)
+      })
+      .attr('cx', (d, i) => {
+        const campusXCoordinate = this.campusXCoordinate(d.campus)
+        const index = this.findIndexOfDataInGroup(d)
+        return this.getX2Coordinate(index, 4, 10) + campusXCoordinate
+      })
       .attr('fill', 'pink')
   }
 
+  findIndexOfDataInGroup = d => {
+    const { data } = this.state
+    const colGroup = data.filter(data => data.campus === d.campus)
+    return colGroup.findIndex(data => data.id === d.id)
+  }
   render() {
     return (
       <section className="page-excl-nav">
